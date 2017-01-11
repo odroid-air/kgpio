5859df941606aaacf10ff1fd1040c433f6230727
Sebastian Kügler <sebas@kde.org>
2017-01-11 11:49:34 +0100

    improve tests
    * test writing to pins more extensively
    * test initial values
    * test lightsensor in different varieties
    
    Only runs correctly on the system with the right breadboard connected,
    but there, it runs without failures.

--- autotests/kgpiotest.cpp
+++ autotests/kgpiotest.cpp
@@ -19,8 +19,12 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 // #include "kgpiotest.h"
 
+
+#include <unistd.h>
+
 #include "kgpio.h"
 #include "gpiopin.h"
+#include "lightsensor.h"
 
 #include <QtTest/QtTest>
 #include <QtCore/QFileInfo>
@@ -32,12 +36,19 @@ class KGpioTest : public QObject
 {
     Q_OBJECT
 
+    QString readFile();
 
 private Q_SLOTS:
     void initTestCase();
 
+    void testRaw();
+
+// public:
     void testPinCreation();
     void testPins();
+    void testPinWrite();
+    void testLowLevelLightSensor();
+    void testLightSensor();
 
     void cleanupTestCase();
 };
@@ -57,15 +68,16 @@ void KGpioTest::cleanupTestCase()
 
 void KGpioTest::testPinCreation()
 {
-    auto pin = KGpio::KGpio::self()->pin(83);
+    return;
+    auto pin = KGpio::KGpio::self()->pin(100);
     QVERIFY(pin != nullptr);
 
-    auto pin83 = KGpio::KGpio::self()->pin(83);
-    auto pin84 = KGpio::KGpio::self()->pin(84);
-    auto pin85 = KGpio::KGpio::self()->pin(85);
-    QCOMPARE(pin, pin83);
-    QVERIFY(pin != pin84);
-    QVERIFY(pin != pin85);
+    auto pin100 = KGpio::KGpio::self()->pin(100);
+    auto pin108 = KGpio::KGpio::self()->pin(108);
+    auto pin97 = KGpio::KGpio::self()->pin(97);
+    QCOMPARE(pin, pin100);
+    QVERIFY(pin != pin108);
+    QVERIFY(pin != pin97);
 
     //QVERIFY(pin->error() == GpioPin::PermissionDenied);
     KGpio::KGpio::self()->setSysfsPath("/blafarghl/rammstein/delta/1337");
@@ -76,13 +88,12 @@ void KGpioTest::testPinCreation()
 
     //auto pin87 = KGpio::KGpio::self()->pin(87);
     //QVERIFY(pin87->error() == GpioPin::PermissionDenied);
-
-
 }
 
 void KGpioTest::testPins()
 {
-    auto pin = KGpio::KGpio::self()->pin(83);
+    return;
+    auto pin = KGpio::KGpio::self()->pin(100);
     QSignalSpy initSpy(pin, &GpioPin::initialized);
 
     QVERIFY(initSpy.wait());
@@ -98,23 +109,122 @@ void KGpioTest::testPins()
         return;
     }
 
+    QCOMPARE(pin->value(), GpioPin::Low);
     while (i <= 10) {
         pin->setValue(GpioPin::High);
+        QCOMPARE(pin->value(), GpioPin::High);
         QTest::qWait(interval);
         pin->setValue(GpioPin::Low);
+        QCOMPARE(pin->value(), GpioPin::Low);
         QTest::qWait(interval);
         i++;
     }
     pin->setValue(GpioPin::High);
-    QTest::qWait(200);
+    QTest::qWait(50);
     pin->setValue(GpioPin::Low);
-    QTest::qWait(200);
+    QTest::qWait(50);
     pin->setValue(GpioPin::High);
-    QTest::qWait(200);
+    QTest::qWait(50);
     pin->setValue(GpioPin::Low);
-    QTest::qWait(200);
+    QTest::qWait(50);
 }
 
+void KGpioTest::testPinWrite()
+{
+    auto pin = KGpio::KGpio::self()->pin(104);
+
+    QSignalSpy initSpy(pin, &GpioPin::initialized);
+
+    pin->setDirection(GpioPin::Out);
+    QCOMPARE(pin->direction(), GpioPin::Out);
+
+    QVERIFY(initSpy.wait());
+    QCOMPARE(pin->value(), GpioPin::Low);
+
+    qDebug() << "pin direction:" << ((pin->direction() == GpioPin::In) ? "In!" : "Out!");
+    pin->setValue(GpioPin::High);
+    QCOMPARE(pin->value(), GpioPin::High);
+
+    pin->setValue(GpioPin::Low);
+    QCOMPARE(pin->value(), GpioPin::Low);
+}
+
+void KGpioTest::testLowLevelLightSensor()
+{
+//     return;
+    auto light = new LightSensor(104, this);
+    light->setDirection(GpioPin::Out);
+    light->setValue(GpioPin::Low);
+    qDebug() << "Light:" << light->direction() << light->value() << light->brightness();
+    QCOMPARE(light->value(), GpioPin::Low);
+
+    light->setDirection(GpioPin::In);
+    QCOMPARE(light->value(), GpioPin::Low);
+    bool go = true;
+    int i = 0;
+    while (go) {
+        i++;
+//         qDebug() << "low..." << light->value();
+        if (light->value() == GpioPin::High) {
+            qDebug() << "Now high!" << i << light->value();
+            go = false;
+        } else {
+            QTest::qWait(3);
+        }
+    }
+
+    QCOMPARE(light->value(), GpioPin::High);
+    light->setDirection(GpioPin::Out);
+    light->setValue(GpioPin::Low);
+    light->deleteLater();
+
+}
+void KGpioTest::testLightSensor()
+{
+    //return;
+    auto light = new LightSensor(104, this);
+    QSignalSpy initSpy(light, &GpioPin::initialized);
+    QVERIFY(initSpy.wait());
+
+    QSignalSpy brightnessSpy(light, &LightSensor::brightnessChanged);
+    QSignalSpy valueSpy(light, &GpioPin::valueChanged);
+
+    QVERIFY(brightnessSpy.wait(5000));
+    qDebug() << "Light ... brightness" << light->brightness();
+    qDebug() << "Light ... value" << light->value();
+
+    QCOMPARE(valueSpy.count(), 1);
+
+    //QTest::qWait(10000);
+}
+
+QString KGpioTest::readFile()
+{
+    QFile f("/sys/devices/virtual/gpio/gpio104/value");
+    f.open(QIODevice::ReadOnly | QIODevice::Text);
+    return f.readAll();
+}
+
+void KGpioTest::testRaw()
+{
+    return;
+    {
+        QFile f("/sys/devices/virtual/gpio/gpio104/value");
+        f.open(QIODevice::WriteOnly | QIODevice::Text);
+        QTextStream stream(&f);
+        stream << 0;
+//         stream << QStringLiteral("0\n");
+        auto  ee = fsync(f.handle());
+        //f.flush();
+        f.close();
+        qDebug() << "Wrote zero to" << f.fileName() << " OK ? " << (ee == 0);
+        QProcess p;
+        p.start("sync");
+        p.waitForFinished();
+
+    }
+    QCOMPARE(readFile(), QStringLiteral("0\n"));
+}
 } // ns
 
 QTEST_GUILESS_MAIN(KGpio::KGpioTest)
